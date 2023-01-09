bd3ac30d71acbfad24917e872e020b3a9605b0b8
code1218 <skjil1218@gmail.com>
2023-01-09 20:09:25 +0900

    아이디 중복확인중

--- src/main/java/com/korit/library/service/AccountService.java
+++ src/main/java/com/korit/library/service/AccountService.java
@@ -1,16 +1,28 @@
 package com.korit.library.service;
 
 import com.korit.library.exception.CustomValidationException;
+import com.korit.library.repository.AccountRepository;
+import com.korit.library.web.dto.UserDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @Service
 public class AccountService {
 
-    public void duplicateUsername(String username) {
+    @Autowired
+    private AccountRepository accountRepository;
 
+    public void duplicateUsername(String username) {
+        UserDto user = accountRepository.findUserByUsername(username);
+        log.info("{}", user);
+        log.info("ROLE_DTL{}", user.getRoleDtlDto());
+        log.info("ROLE_MST{}", user.getRoleDtlDto().get(0));
+        log.info("ROLE_MST{}", user.getRoleDtlDto().get(1));
     }
 
     public void compareToPassword(String password, String repassword) {
--- src/main/java/com/korit/library/web/api/AccountApi.java
+++ src/main/java/com/korit/library/web/api/AccountApi.java
@@ -1,8 +1,10 @@
 package com.korit.library.web.api;
 
 import com.korit.library.aop.annotation.ValidAspect;
+import com.korit.library.service.AccountService;
 import com.korit.library.web.dto.CMRespDto;
 import com.korit.library.web.dto.UserDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,9 +18,15 @@ import javax.validation.Valid;
 @RequestMapping("/api/account")
 public class AccountApi {
 
+    @Autowired
+    private AccountService accountService;
+
     @ValidAspect
     @PostMapping("/register")
     public ResponseEntity<?> register(@RequestBody @Valid UserDto userDto, BindingResult bindingResult) {
+
+        accountService.duplicateUsername(userDto.getUsername());
+
 
         return ResponseEntity
                 .created(null)
