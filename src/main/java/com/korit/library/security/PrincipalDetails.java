8e72a47b7bca51232808d809fd66137e7f485d83
code1218 <skjil1218@gmail.com>
2023-01-11 19:55:41 +0900

    로그인 성공

--- src/main/java/com/korit/library/security/PrincipalDetails.java
+++ src/main/java/com/korit/library/security/PrincipalDetails.java
@@ -1,5 +1,7 @@
 package com.korit.library.security;
 
+import com.korit.library.web.dto.RoleDtlDto;
+import com.korit.library.web.dto.RoleMstDto;
 import com.korit.library.web.dto.UserDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -9,6 +11,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 
 @RequiredArgsConstructor
@@ -20,13 +23,29 @@ public class PrincipalDetails implements UserDetails {
     private Map<String, Object> response;
 
 
+    // 권한을 리스트로 관리하는 부분
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         ArrayList<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
 
-        user.getRoleDtlDto().forEach(dtl -> {
-            authorities.add(() -> dtl.getRoleMstDto().getRoleName());
-        });
+        List<RoleDtlDto> roleDtlDtoList = user.getRoleDtlDto();
+        for(int i = 0; i < roleDtlDtoList.size(); i++) {
+            RoleDtlDto dtl = roleDtlDtoList.get(i); // 0 = ROLE_USER, 1 = ROLE_ADMIN
+            RoleMstDto roleMstDto = dtl.getRoleMstDto();
+            String roleName = roleMstDto.getRoleName();
+
+            GrantedAuthority role = new GrantedAuthority() {
+                @Override
+                public String getAuthority() {
+                    return roleName;
+                }
+            };
+            authorities.add(role);
+        }
+
+//        user.getRoleDtlDto().forEach(dtl -> {
+//            authorities.add(() -> dtl.getRoleMstDto().getRoleName());
+//        });
 
         return authorities;
     }
@@ -41,21 +60,30 @@ public class PrincipalDetails implements UserDetails {
         return user.getUsername();
     }
 
+    /*
+        계정 만료 여부
+     */
     @Override
     public boolean isAccountNonExpired() {
         return true;
     }
-
+    /*
+        계정 잠김 여부
+    */
     @Override
     public boolean isAccountNonLocked() {
         return true;
     }
-
+    /*
+        비밀번호 만료 여부
+     */
     @Override
     public boolean isCredentialsNonExpired() {
         return true;
     }
-
+    /*
+        사용자 활성화 여부
+     */
     @Override
     public boolean isEnabled() {
         return true;
