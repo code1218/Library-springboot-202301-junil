0fb219e6539b1d6cb14182c8706a662857fa7544
code1218 <skjil1218@gmail.com>
2023-01-17 19:53:05 +0900

    도서 이미지 조회, 삭제

--- src/main/java/com/korit/library/repository/BookRepository.java
+++ src/main/java/com/korit/library/repository/BookRepository.java
@@ -36,6 +36,8 @@ public interface BookRepository {
     public int deleteBook(String bookCode);
 
     public int registerBookImages(List<BookImageDto> bookImageDtos);
+    public List<BookImageDto> findBookImageAll(String bookCode);
+    public int deleteBookImage(int imageId);
 }
 
 
--- src/main/java/com/korit/library/service/BookService.java
+++ src/main/java/com/korit/library/service/BookService.java
@@ -99,7 +99,14 @@ public class BookService {
         });
 
         bookRepository.registerBookImages(bookImageDtos);
+    }
 
+    public List<BookImageDto> getBooks(String bookCode) {
+        return bookRepository.findBookImageAll(bookCode);
+    }
+
+    public void removeBookImage(int imageId) {
+        bookRepository.deleteBookImage(imageId);
     }
 }
 
