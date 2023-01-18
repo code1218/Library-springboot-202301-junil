d5d3ab89fc45484e03bd20b6512babf2a96c6b97
code1218 <skjil1218@gmail.com>
2023-01-18 19:52:42 +0900

    library db수정, rental start

---
+++ src/main/java/com/korit/library/repository/RentalRepository.java
@@ -0,0 +1,10 @@
+package com.korit.library.repository;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface RentalRepository {
+
+    public int rentalAvailability(int userId);
+
+}
---
+++ src/main/java/com/korit/library/service/RentalService.java
@@ -0,0 +1,33 @@
+package com.korit.library.service;
+
+import com.korit.library.exception.CustomRentalException;
+import com.korit.library.repository.RentalRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class RentalService {
+
+    @Autowired
+    private RentalRepository rentalRepository;
+
+    public void rental(int userId, int bookId) {
+
+        availability(userId);
+
+    }
+
+    private void availability(int userId) {
+        int rental_count = rentalRepository.rentalAvailability(userId);
+        if(rental_count > 2) {
+            Map<String, String> errorMap = new HashMap<>();
+            errorMap.put("rentalCountError", "대여 회수를 초과하였습니다.");
+            throw new CustomRentalException(errorMap);
+        }
+    }
+
+
+}
