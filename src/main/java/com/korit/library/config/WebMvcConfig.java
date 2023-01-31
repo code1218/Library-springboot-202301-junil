144ece7545e7b9a38dcb34488f00285aafa19e54
code1218 <skjil1218@gmail.com>
2023-01-31 19:49:54 +0900

    도서 등록 완료

--- src/main/java/com/korit/library/config/WebMvcConfig.java
+++ src/main/java/com/korit/library/config/WebMvcConfig.java
@@ -20,7 +20,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("*").allowedOrigins("http://172.25.176.1:5500");
+        registry.addMapping("/**").allowedMethods("*").allowedOrigins("*");
     }
 
     @Override
